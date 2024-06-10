c1f9831c0650da8a8a60a4da67b0f400cce479f1
DiegoR-Lima <ediegorodrigo@outlook.com>
2024-06-10 15:24:56 -0300

    Frist Commit

---
+++ EcoLife/EcoLife/src/main/java/EcoLifeV1/EcoLife.java
@@ -0,0 +1,12 @@
+package EcoLifeV1;
+
+/**
+ *
+ * @author Diego Rodrigo
+ */
+public class EcoLife {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
